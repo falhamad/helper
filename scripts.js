84306efb4e2fcd40cbe186c7d328b7663e765c9c
fatma al hamad <fatmaalhamadd@gmail.com>
2020-10-24 20:53:03 +0300

    مشروع

--- scripts.js
+++ scripts.js
@@ -30,7 +30,3 @@ document.getElementById("form1").onsubmit=function() {
 return false; // required to not refresh the page; just leave this here
 } //this ends the submit function
 
-//pic changes
-
-
-
